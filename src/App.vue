f3260a78a5dd27c3e153f1fe530675983c99e5b9
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2019-03-24 02:13:35 -0500

    `TinyProgressBar` component creation.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <TinyProgressBar></TinyProgressBar>
+  </div>
 </template>
 
 <script>
+import TinyProgressBar from './components/TinyProgressBar.vue'
+
 export default {
   name: 'app',
-  components: {}
+  components: {
+    TinyProgressBar
+  }
 }
 </script>
 
 <style>
-
+html, body {
+  margin: 0;
+}
 </style>
---
+++ src/components/TinyProgressBar.vue
@@ -0,0 +1,66 @@
+
+<template>
+  <div class="bar" :style="styledBar">
+    <div class="progress" :style="styledProgress"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TinyProgressBar',
+  props: {
+    color: {
+      type: String, 
+      default: "#1ed760"
+    }, 
+    height: {
+      type: Number, 
+      default: 2
+    }
+  },
+  data() {
+    return {
+      progress: 0
+    }
+  },
+  methods: {
+    setProgress: function (progress) {
+      if (isNaN(progress)) {
+        return;
+      }
+
+      if (progress < 0) {
+        this.progress = 0;
+      } else if (progress > 100) {
+        this.progress = 100;
+      } else {
+        this.progress = progress;
+      }
+    }
+  },
+  computed: {
+    styledBar: function () {
+      return {
+        'height': this.height + 'px'
+      }
+    },
+    styledProgress: function () {
+      return {
+        'width': this.progress + '%', 
+        'background-color': this.color
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bar {
+  width: 100%;
+  overflow: hidden;
+}
+.progress {
+  width: 100%;
+  height: 100%;
+}
+</style>
